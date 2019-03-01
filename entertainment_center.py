a393a618553acaa0058aef57e0ec5259a27fbd5f
LavanyaMoyya <47386515+LavanyaMoyya@users.noreply.github.com>
2019-03-01 10:51:01 +0530

    Add files via upload

---
+++ entertainment_center.py
@@ -0,0 +1,26 @@
+import media
+import fresh_tomatoes
+
+Agnathavaasi = media.Movie("Agnathavaasi", "Agnathavaasi",
+                  "https://i.pinimg.com/originals/9f/2b/af/9f2baf839742659396c815315b6462c6.jpg",
+                  "https://www.youtube.com/watch?v=97h9fBWltBM")
+
+Aravinda Sametha Veera Raghava = media.Movie("Aravinda Sametha Veera Raghava", "Factionism",
+                  "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Aravinda_Sametha_Veera_Raghava_Poster.jpg/220px-Aravinda_Sametha_Veera_Raghava_Poster.jpg",
+                  "https://www.youtube.com/watch?v=dNMe5oRfsCE&t=63s")
+
+Mahanati = media.Movie("Mahanati", "Actress Savitri Biopic",
+                  "https://in.bmscdn.com/iedb/movies/images/website/poster/large/mahanati-et00054544-09-03-2017-05-04-00.jpg",
+                  "https://www.youtube.com/watch?v=PLmBpf7UHJs")
+
+Geetha Govindam = media.Movie("Geetha Govindam", "Love story",
+                  "https://www.mirchi9.com/wp-content/gallery/geetha-govindam-movie-posters/geetha-govindam-movie-posters-2.jpg",
+                  "https://www.youtube.com/watch?v=U3dqoAHqagk")
+
+Goodachari = media.Movie("Goodachari", "Thriller",
+                  "https://moviegalleri.net/wp-content/uploads/2018/08/Adivi-Sesh-Goodachari-Movie-Releasing-Today-Posters.jpg",
+                  "https://www.youtube.com/watch?v=V-8GGhn0oDk")
+
+C/o.Kancharapalem = media.Movie("C/o.Kancharapalem", "Village Love story",
+                  "https://m.media-amazon.com/images/M/MV5BOGRjY2RiMTYtZGZlMC00NzQ3LWEyODUtOTcwNWMwM2JkNjJmXkEyXkFqcGdeQXVyMjk5MDYzMDA@._V1_UY268_CR1,0,182,268_AL_.jpg",           
+                  "https://www.youtube.com/watch?v=SPG3L0s5dw0")
